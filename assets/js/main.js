c5b2522d6bb4af2633db285ee35e1c53dfee7035
jt <jainiltimaniya5967@gmail.com>
2024-06-21 16:29:09 +0530

    removed wapp default msg

--- assets/js/main.js
+++ assets/js/main.js
@@ -443,7 +443,6 @@ Description: Gerold - Personal Portfolio HTML5 Template
 // jainil js
 function openWhatsApp() {
     const phoneNumber = "9328767877"; 
-    const message = "Hello, jainil this side."; 
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
